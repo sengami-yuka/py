87149e40e519d73573a95a3a998fdd50b350139b
sengami-yuka <sengamiyuka@qq.com>
2022-02-27 02:18:01 +0800

    lc

---
+++ coding_problems/leetcode/interview_01_02_check_permutation_lcci.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+
+class Solution(object):
+    def CheckPermutation(self, s1, s2):
+        return sorted(s1) == sorted(s2)
+
+    def CheckPermutation2(self, s1, s2):
+        d = defaultdict(int)
+        for c in s1:
+            d[c] += 1
+        for c in s2:
+            d[c] -= 1
+        return all(x == 0 for x in d.values())
