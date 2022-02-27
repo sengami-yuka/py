44842555193db698677cd29c9a667f2a63706695
sengami-yuka <sengamiyuka@qq.com>
2022-02-27 23:56:16 +0800

    lc

---
+++ coding_problems/leetcode/lcci_0103_string_to_url.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def replaceSpaces(self, S, length):
+        return S[:length].replace(' ', '%20')
+
+
+s = Solution()
+assert 'Mr%20John%20Smith' == s.replaceSpaces('Mr John Smith ', 13)
+assert '%20%20%20%20%20' == s.replaceSpaces('               ', 5)
