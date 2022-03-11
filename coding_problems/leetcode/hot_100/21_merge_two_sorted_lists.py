dd0c79e97f155b3d33d73c080968ed1bdab558da
sengami-yuka <sengamiyuka@qq.com>
2022-03-11 22:19:56 +0800

    lc

---
+++ coding_problems/leetcode/hot_100/21_merge_two_sorted_lists.py
@@ -0,0 +1,83 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __repr__(self):
+        a = []
+        cur = self
+        while cur:
+            a.append(cur.val)
+            cur = cur.next
+        return str(a)
+
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        if list1.val > list2.val:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
+        else:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+
+
+class Solution2:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        if list1.val < list2.val:
+            head = list1
+            list1 = list1.next
+        else:
+            head = list2
+            list2 = list2.next
+        cur = head
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1 = list1.next
+            else:
+                cur.next = list2
+                list2 = list2.next
+            cur = cur.next
+        if list1:
+            cur.next = list1
+        elif list2:
+            cur.next = list2
+        return head
+
+
+class Solution3:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        prehead = ListNode()
+
+        prev = prehead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+
+        prev.next = l1 if l1 is not None else l2
+
+        return prehead.next
+
+
+solution = Solution3()
+a = ListNode(1, ListNode(2, ListNode(4)))
+b = ListNode(1, ListNode(3, ListNode(4)))
+print(solution.mergeTwoLists(a, b))
