a1c2b505c9a3f0d9e2ea5e016ca96afafd2e162e
sengami-yuka <sengamiyuka@qq.com>
2022-03-12 15:36:29 +0800

    lc

---
+++ coding_problems/leetcode/lcof_09.py
@@ -0,0 +1,26 @@
+class CQueue:
+
+    def __init__(self):
+        self.a = []
+        self.b = []
+
+    def appendTail(self, value: int) -> None:
+        self.a.append(value)
+
+    def deleteHead(self) -> int:
+        if self.b:
+            return self.b.pop()
+        while self.a:
+            self.b.append(self.a.pop())
+        if self.b:
+            return self.b.pop()
+        return -1
+
+
+obj = CQueue()
+for i in range(10):
+    obj.appendTail(i)
+print(obj.a, obj.b)
+for i in range(10):
+    param_2 = obj.deleteHead()
+    print(param_2)
