8ab8738bc88b339e36c217434abf26f2b6c12c72
sengami-yuka <sengamiyuka@qq.com>
2022-03-15 23:00:45 +0800

    lc

---
+++ coding_problems/leetcode/hot_100/98_validate_binary_search_tree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        def dfs(node):
+            nonlocal pre
+            if node:
+                dfs(node.left)
+                if pre >= node.val:
+                    raise RuntimeError
+                pre = node.val
+                dfs(node.right)
+        pre = float('-inf')
+        try:
+            dfs(root)
+        except RuntimeError:
+            return False
+        return True
+
+
+solution = Solution()
+root = TreeNode(2, TreeNode(1), TreeNode(3))
+assert solution.isValidBST(root)
+
+root = TreeNode(5, TreeNode(1), TreeNode(4, TreeNode(3), TreeNode(6)))
+assert not solution.isValidBST(root)
