aaa3f6a871f15a0ef67bc9b5957de76bfb929536
sengami-yuka <sengamiyuka@qq.com>
2022-03-16 22:48:21 +0800

    lc

---
+++ coding_problems/leetcode/lcof/lcof_32_i.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+import collections
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        ans = []
+        q = collections.deque()
+        q.append(root)
+        while q:
+            node = q.popleft()
+            ans.append(node.val)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        return ans
