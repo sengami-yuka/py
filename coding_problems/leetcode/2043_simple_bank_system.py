efac42d6c4850964362c5cefefa1f8ab7883033e
sengami-yuka <sengamiyuka@qq.com>
2022-03-18 20:33:49 +0800

    lc

---
+++ coding_problems/leetcode/2043_simple_bank_system.py
@@ -0,0 +1,69 @@
+from typing import List
+
+
+class Bank:
+
+    def __init__(self, balance: List[int]):
+        self.balance = balance
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if account1 > len(self.balance) or account2 > len(self.balance):
+            return False
+        if self.balance[account1 - 1] < money:
+            return False
+        self.balance[account1 - 1] -= money
+        self.balance[account2 - 1] += money
+        return True
+
+    def deposit(self, account: int, money: int) -> bool:
+        if account > len(self.balance):
+            return False
+        self.balance[account - 1] += money
+        return True
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if account > len(self.balance):
+            return False
+        if self.balance[account - 1] < money:
+            return False
+        self.balance[account - 1] -= money
+        return True
+
+
+class Bank2:
+
+    def __init__(self, balance: List[int]):
+        self.balance = {k: v for k, v in enumerate(balance, 1)}
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if account1 > len(self.balance) or account2 > len(self.balance):
+            return False
+        if self.balance[account1] < money:
+            return False
+        self.balance[account1] -= money
+        self.balance[account2] += money
+        return True
+
+    def deposit(self, account: int, money: int) -> bool:
+        if account > len(self.balance):
+            return False
+        self.balance[account] += money
+        return True
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if account > len(self.balance):
+            return False
+        if self.balance[account] < money:
+            return False
+        self.balance[account] -= money
+        return True
+
+
+ipt1 = ["Bank", "withdraw", "transfer", "deposit", "transfer", "withdraw"]
+ipt2 = [[[10, 100, 20, 50, 30]], [3, 10], [5, 1, 20], [5, 20], [3, 4, 15], [10, 50]]
+out = [None, True, True, True, False, False]
+
+bank = Bank2(ipt2[0][0])
+for i in range(1, len(ipt1)):
+    ans = bank.__getattribute__(ipt1[i])(*ipt2[i])
+    assert ans == out[i], ans
