8c641c6b195316e45dcf6dbbf40e74fa0252d89c
sengami-yuka <sengamiyuka@qq.com>
2022-03-25 21:35:53 +0800

    lc

--- coding_problems/leetcode/172_factorial_trailing_zeroes.py
+++ coding_problems/leetcode/172_factorial_trailing_zeroes.py
@@ -2,6 +2,6 @@ class Solution:
     def trailingZeroes(self, n: int) -> int:
         ans = 0
         while n:
-            ans += n // 5
             n //= 5
+            ans += n
         return ans
