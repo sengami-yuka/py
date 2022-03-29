75d307f082461f9bde77fb51b959b9dcf6506122
sengami-yuka <sengamiyuka@qq.com>
2022-03-29 23:37:56 +0800

    lc

--- coding_problems/leetcode/2024_maximize_the_confusion_of_an_exam.py
+++ coding_problems/leetcode/2024_maximize_the_confusion_of_an_exam.py
@@ -1,3 +1,6 @@
+import collections
+
+
 class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
         def f(key):
@@ -13,7 +16,36 @@ class Solution:
         return max(f('T'), f('F'))
 
 
-solution = Solution()
+class Solution2:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        ans = left = cnts_t = cnts_f = 0
+        for right, c in enumerate(answerKey):
+            cnts_t += c == 'T'
+            cnts_f += c == 'F'
+            # 当前区间内的t和f的个数不能都大于k，我们只能变k次
+            while cnts_t > k and cnts_f > k:
+                # 超过最大变动次数了，当前指针的区间最左要向右移动
+                cnts_t -= answerKey[left] == 'T'
+                cnts_f -= answerKey[left] == 'F'
+                left += 1
+            ans = max(ans, right - left + 1)
+        return ans
+
+
+class Solution3:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        left = cnts_t = cnts_f = 0
+        for right, c in enumerate(answerKey):
+            cnts_t += c == 'T'
+            cnts_f += c == 'F'
+            if cnts_t > k and cnts_f > k:
+                cnts_t -= answerKey[left] == 'T'
+                cnts_f -= answerKey[left] == 'F'
+                left += 1
+        return right - left + 1
+
+
+solution = Solution3()
 ans = solution.maxConsecutiveAnswers('TTFF', 2)
 assert ans == 4, ans
 
