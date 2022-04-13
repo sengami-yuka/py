1859ab5210c3aa27646ac89576ffecd291a171ea
sengami-yuka <sengamiyuka@qq.com>
2022-04-14 01:20:01 +0800

    lc

--- coding_problems/leetcode/1672_richest_customer_wealth.py
+++ coding_problems/leetcode/1672_richest_customer_wealth.py
@@ -7,3 +7,8 @@ class Solution:
         for row in accounts:
             ans = max(ans, sum(row))
         return ans
+
+
+class Solution2:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return max(map(sum, accounts))
