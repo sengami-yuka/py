86fbf911978017153945fb2e080ff0b92e076366
sengami-yuka <sengamiyuka@qq.com>
2022-04-14 01:19:36 +0800

    lc

---
+++ coding_problems/leetcode/1672_richest_customer_wealth.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        ans = 0
+        for row in accounts:
+            ans = max(ans, sum(row))
+        return ans
