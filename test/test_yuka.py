8649400457db14217f0f4776ae06497184048686
sengami-yuka <sengamiyuka@qq.com>
2023-08-22 18:04:01 +0800

    1

--- test/test_yuka.py
+++ test/test_yuka.py
@@ -5,6 +5,9 @@ class TestYuka(unittest.TestCase):
     def test_yuka(self):
         self.assertEqual(True, False)
 
+    def test_yuka2(self):
+        self.assertEqual(1, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
