7b2ef4f2f9ae01a391c62d22d4da2fa09a0f049a
sengami-yuka <sengamiyuka@qq.com>
2024-04-01 15:06:36 +0800

    feat2

--- test/test_yuka.py
+++ test/test_yuka.py
@@ -11,6 +11,9 @@ class TestYuka(unittest.TestCase):
     def test_yuka3(self):
         self.assertLess(1, 2)
 
+    def test_yuka5(self):
+        self.assertGreaterEqual(44, 33)
+
 
 if __name__ == '__main__':
     unittest.main()
